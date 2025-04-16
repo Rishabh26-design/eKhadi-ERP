f730391c96b4fa25e40c062c7fca03062872d3fc
Rishabh26-design <rishabhsurya1c8349@gmail.com>
2025-04-16 18:47:48 +0530

    ok

---
+++ eKhadi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eKhadi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
